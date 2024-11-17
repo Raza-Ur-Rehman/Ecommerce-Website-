cc6e0938ad99235440b26f902540ce78e3a82e02
Raza-Ur-Rehman <mrazaurrehman12345@gmail.com>
2024-11-17 18:38:37 +0500

    code :construction:

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,10 +1,316 @@
 
+
 const Footer = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <>
+      <footer className="border-b-4 border-heading mt-9 md:mt-11 lg:mt-16 3xl:mt-20 pt-2.5 lg:pt-0 2xl:pt-2">
+        <div className="mx-auto max-w-[1920px] px-4 md:px-8 2xl:px-16">
+          <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 md:gap-9 xl:gap-5  pb-9 md:pb-14 lg:pb-16 2xl:pb-20 3xl:pb-24 lg:mb-0.5 2xl:mb-0 3xl:-mb-1 xl:grid-cols-6">
+            <div className="pb-3 md:pb-0">
+              <h4 className="mb-5 text-sm font-semibold text-heading md:text-base xl:text-lg 2xl:mb-6 3xl:mb-7">
+                Social
+              </h4>
+              <ul className="text-xs lg:text-sm text-body flex flex-col space-y-3 lg:space-y-3.5">
+                <li className="flex items-baseline">
+                  <span className="ltr:mr-3 rtl:ml-3 relative top-0.5 lg:top-1 text-sm lg:text-base">
+                  <i className="fa-brands fa-instagram"></i>
+                    
+                  </span>
+                  <a
+                    className="transition-colors duration-200 hover:text-black"
+                    href="https://www.instagram.com/redqinc/"
+                  >
+                    Instagram
+                  </a>
+                </li>
+                <li className="flex items-baseline">
+                  <span className="ltr:mr-3 rtl:ml-3 relative top-0.5 lg:top-1 text-sm lg:text-base">
+                    
+                  </span>
+                  <a
+                    className="transition-colors duration-200 hover:text-black"
+                    href="https://twitter.com/redqinc"
+                  >
+                    Twitter
+                  </a>
+                </li>
+                <li className="flex items-baseline">
+                  <span class="ltr:mr-3 rtl:ml-3 relative top-0.5 lg:top-1 text-sm lg:text-base">
+                   
+                  </span>
+                  <a
+                    className="transition-colors duration-200 hover:text-black"
+                    href="https://www.facebook.com/redqinc/"
+                  >
+                    Facebook
+                  </a>
+                </li>
+                <li className="flex items-baseline">
+                  <span className="ltr:mr-3 rtl:ml-3 relative top-0.5 lg:top-1 text-sm lg:text-base">
+                   
+                  </span>
+                  <a
+                    className="transition-colors duration-200 hover:text-black"
+                    href="https://www.youtube.com/channel/UCjld1tyVHRNy_pe3ROLiLhw"
+                  >
+                    Youtube
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <div class="pb-3 md:pb-0 ">
+              <h4 class="mb-5 text-sm font-semibold text-heading md:text-base xl:text-lg 2xl:mb-6 3xl:mb-7">
+                Contact
+              </h4>
+              <ul class="text-xs lg:text-sm text-body flex flex-col space-y-3 lg:space-y-3.5">
+                <li class="flex items-baseline">
+                  <a
+                    class="transition-colors duration-200 hover:text-black"
+                    href="/contact-us"
+                  >
+                    Contact Us
+                  </a>
+                </li>
+                <li class="flex items-baseline">
+                  <a
+                    class="transition-colors duration-200 hover:text-black"
+                    href="/"
+                  >
+                    yourexample@email.com
+                  </a>
+                </li>
+                <li class="flex items-baseline">
+                  <a
+                    class="transition-colors duration-200 hover:text-black"
+                    href="/"
+                  >
+                    example@email.com
+                  </a>
+                </li>
+                <li class="flex items-baseline">
+                  <a
+                    class="transition-colors duration-200 hover:text-black"
+                    href="/"
+                  >
+                    Call us: +1 254 568-5479
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <div class="pb-3 md:pb-0 ">
+              <h4 class="mb-5 text-sm font-semibold text-heading md:text-base xl:text-lg 2xl:mb-6 3xl:mb-7">
+                About
+              </h4>
+              <ul class="text-xs lg:text-sm text-body flex flex-col space-y-3 lg:space-y-3.5">
+                <li class="flex items-baseline">
+                  <a
+                    class="transition-colors duration-200 hover:text-black"
+                    href="/contact-us"
+                  >
+                    Support Center
+                  </a>
+                </li>
+                <li class="flex items-baseline">
+                  <a
+                    class="transition-colors duration-200 hover:text-black"
+                    href="/"
+                  >
+                    Customer Support
+                  </a>
+                </li>
+                <li class="flex items-baseline">
+                  <a
+                    class="transition-colors duration-200 hover:text-black"
+                    href="/contact-us"
+                  >
+                    About Us
+                  </a>
+                </li>
+                <li class="flex items-baseline">
+                  <a
+                    class="transition-colors duration-200 hover:text-black"
+                    href="/"
+                  >
+                    Copyright
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <div class="pb-3 md:pb-0 ">
+              <h4 class="mb-5 text-sm font-semibold text-heading md:text-base xl:text-lg 2xl:mb-6 3xl:mb-7">
+                Customer Care
+              </h4>
+              <ul class="text-xs lg:text-sm text-body flex flex-col space-y-3 lg:space-y-3.5">
+                <li class="flex items-baseline">
+                  <a
+                    class="transition-colors duration-200 hover:text-black"
+                    href="/faq"
+                  >
+                    FAQ &amp; Helps
+                  </a>
+                </li>
+                <li class="flex items-baseline">
+                  <a
+                    class="transition-colors duration-200 hover:text-black"
+                    href="/"
+                  >
+                    Shipping &amp; Delivery
+                  </a>
+                </li>
+                <li class="flex items-baseline">
+                  <a
+                    class="transition-colors duration-200 hover:text-black"
+                    href="/"
+                  >
+                    Return &amp; Exchanges
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <div class="pb-3 md:pb-0 ">
+              <h4 class="mb-5 text-sm font-semibold text-heading md:text-base xl:text-lg 2xl:mb-6 3xl:mb-7">
+                Our Information
+              </h4>
+              <ul class="text-xs lg:text-sm text-body flex flex-col space-y-3 lg:space-y-3.5">
+                <li class="flex items-baseline">
+                  <a
+                    class="transition-colors duration-200 hover:text-black"
+                    href="/privacy"
+                  >
+                    Privacy policy update
+                  </a>
+                </li>
+                <li class="flex items-baseline">
+                  <a
+                    class="transition-colors duration-200 hover:text-black"
+                    href="/terms"
+                  >
+                    Terms &amp; conditions
+                  </a>
+                </li>
+                <li class="flex items-baseline">
+                  <a
+                    class="transition-colors duration-200 hover:text-black"
+                    href="/privacy"
+                  >
+                    Return Policy
+                  </a>
+                </li>
+                <li class="flex items-baseline">
+                  <a
+                    class="transition-colors duration-200 hover:text-black"
+                    href="/"
+                  >
+                    Site Map
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <div class="pb-3 md:pb-0 ">
+              <h4 class="mb-5 text-sm font-semibold text-heading md:text-base xl:text-lg 2xl:mb-6 3xl:mb-7">
+                Top Categories
+              </h4>
+              <ul class="text-xs lg:text-sm text-body flex flex-col space-y-3 lg:space-y-3.5">
+                <li class="flex items-baseline">
+                  <a
+                    class="transition-colors duration-200 hover:text-black"
+                    href="/search"
+                  >
+                    Men's Wear
+                  </a>
+                </li>
+                <li class="flex items-baseline">
+                  <a
+                    class="transition-colors duration-200 hover:text-black"
+                    href="/search"
+                  >
+                    Men's Wear
+                  </a>
+                </li>
+                <li class="flex items-baseline">
+                  <a
+                    class="transition-colors duration-200 hover:text-black"
+                    href="/search"
+                  >
+                    Kid's Wear
+                  </a>
+                </li>
+                <li class="flex items-baseline">
+                  <a
+                    class="transition-colors duration-200 hover:text-black"
+                    href="/search"
+                  >
+                    Sports Wear
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div class="pt-5 pb-16 mb-2 border-t border-gray-300 sm:pb-20 md:pb-5 sm:mb-0">
+          <div class="flex flex-col-reverse md:flex-row text-center md:justify-between mx-auto max-w-[1920px] px-4 md:px-8 2xl:px-16">
+            <p class="text-body text-xs lg:text-sm leading-6">
+              Copyright© 2024&nbsp;
+              <a
+                class="font-semibold text-gray-700 transition-colors duration-200 ease-in-out hover:text-body"
+                href="https://redq.io"
+              >
+                REDQ
+              </a>
+              &nbsp; All rights reserved
+            </p>
+            <ul class="flex-wrap items-center justify-center hidden mx-auto mb-1 md:flex gap-x-4 xs:gap-x-5 lg:gap-x-7 md:mb-0 md:mx-0">
+              <li class="mb-2 transition md:mb-0 hover:opacity-80 ">
+                <a href="/" target="_blank">
+                  <img
+                    src="/assets/img/paymentIcon/masterCard.png"
+                    alt="Master Card"
+                    className="w-[34px] h-[20px] "
+                  />
+                </a>
+              </li>
+              <li class="mb-2 transition md:mb-0 hover:opacity-80 ">
+                <a href="/" target="_blank">
+                  <img
+                    src="/assets/img/paymentIcon/visa.png"
+                    alt="Visa"
+                   className="w-[50px] h-[20px] object-contain"
+                  />
+                </a>
+              </li>
+              <li class="mb-2 transition md:mb-0 hover:opacity-80 ">
+                <a href="/" target="_blank">
+                  <img
+                    src="/assets/img/paymentIcon/paypal.png"
+                    alt="Paypal"
+                    className="w-[76px] h-[20px] object-contain"
+                  />
+                </a>
+              </li>
+              <li class="mb-2 transition md:mb-0 hover:opacity-80 ">
+                <a href="/" target="_blank">
+                  <img
+                    src="/assets/img/paymentIcon/jcb.png"
+                    alt="JCB"
+                    className="w-[26px] h-[20px] object-contain"
+                  />
+                </a>
+              </li>
+              <li class="mb-2 transition md:mb-0 hover:opacity-80 ">
+                <a href="/" target="_blank">
+                  <img
+                    src="/assets/img/paymentIcon/skrill.png"
+                    alt="Skrill"
+                    className="w-[40px] h-[20px] object-contain"
+                  />
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </footer>
+    </>
+  );
+};
 
-export default Footer
+export default Footer;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,7 @@ import Card from "../Components/common/Card";
 import Header from "../Components/Header";
 import { ProductContext } from "../Context/ProductContext";
 import Banner from "../Components/Banner";
+import Footer from "../Components/Footer";
 
 import { FeaturedProductContext } from "../Context/FeaturedProductsContext";
 import DualBanner from "../Components/DualBanner";
@@ -81,6 +82,10 @@ const Home = () => {
   // console.log(isfilter);
   const [modalShow, setModalShow] = useState(false);
 
+  const style = {
+    backgroundImage: "url(/assets/img/subscription-bg.png)",
+  };
+
   return (
     <div className=" w-full px-4">
       <div className=" py-3">
@@ -106,7 +111,10 @@ const Home = () => {
                 (item, key) => {
                   const { name, image } = item;
                   return (
-                    <div className="catagaryCard " key={key}>
+                    <div
+                      className="catagaryCard hover:-translate-y-2"
+                      key={key}
+                    >
                       <Card
                         title={name}
                         description=""
@@ -131,7 +139,7 @@ const Home = () => {
                 const { title, description, image, price } = item;
                 return (
                   <div
-                    className="FlashCard"
+                    className="FlashCard hover:-translate-y-2"
                     key={key}
                     onClick={() => findId(item.id)}
                   >
@@ -162,10 +170,12 @@ const Home = () => {
           <div className="flex flex-wrap p-4 gap-3 justify-center ">
             {productData?.products?.slice(0, 12).map((item, key) => {
               const { title, description, images, price } = item;
-              const arrivalCard =
-                "arrivalCard md:w-[46%] lg:w-[30%] xl:w-[24%] ";
               return (
-                <div className={arrivalCard} key={key} onClick={() => {}}>
+                <div
+                  className="arrivalCard md:w-[46%] lg:w-[30%] xl:w-[24%] hover:-translate-y-2"
+                  key={key}
+                  onClick={() => {}}
+                >
                   <Card
                     title={title.slice(0, 22) + "..."}
                     description={description.slice(0, 33) + "..."}
@@ -188,7 +198,7 @@ const Home = () => {
               const { title, description, images, price } = item;
               return (
                 <div
-                  className="brandCard  md:w-[30%] lg:w-[20%] xl:w-[15%]"
+                  className="brandCard  md:w-[30%] lg:w-[20%] xl:w-[15%] hover:-translate-y-2"
                   key={key}
                 >
                   <Card title={title.slice(0, 10)} images={images} />
@@ -204,51 +214,70 @@ const Home = () => {
           </h1>
           <div className="slider-container px-10  ">
             <Slider {...settings}>
-                {testimonialsData?.map((item, key) => {
-                  const { name, description, image } = item;
-                  return (
-                    <div className="p-3">
-                      <div className="testimonalCard" key={key}>
+              {testimonialsData?.map((item, key) => {
+                const { name, description, image } = item;
+                return (
+                  <div className="p-3">
+                    <div
+                      className="testimonalCard hover:-translate-y-2"
+                      key={key}
+                    >
                       <Card
                         title={name}
                         description={description}
                         images={image}
                       />
                     </div>
-                    </div>
-                  );
-                })}
-              
+                  </div>
+                );
+              })}
             </Slider>
           </div>
-
+        </div>
+        {/* subscription  */}
+        <div className="my-2 py-5">
+        <div className="px-5 sm:px-8 md:px-16 2xl:px-24 relative overflow-hidden flex flex-col sm:items-center xl:items-start rounded-lg bg-gray-200 py-10 md:py-14 lg:py-16">
+          <div className="-mt-1.5 lg:-mt-2 xl:-mt-0.5 text-center ltr:xl:text-left rtl:xl:text-right mb-7 md:mb-8 lg:mb-9 xl:mb-0">
+            <h3 className="text-lg md:text-xl lg:text-2xl 2xl:text-3xl xl:leading-10 font-bold text-heading mb-2 md:mb-2.5 lg:mb-3 xl:mb-3.5">
+              Get Expert Tips In Your Inbox
+            </h3>
+            <p className="text-xs leading-6 text-body md:text-sm md:leading-7">
+              Subscribe to our newsletter and stay updated.
+            </p>
+          </div>
+          <form className="flex-shrink-0 w-full sm:w-96 md:w-[545px] md:mt-7 z-10">
+            <div class="flex flex-col items-start justify-end sm:flex-row">
+              <div class="w-full">
+                <input
+                  id="subscription_email"
+                  name="subscription_email"
+                  type="email"
+                  placeholder="Write your email here"
+                  className="py-2 px-4 md:px-5 w-full appearance-none transition duration-150 ease-in-out border text-input text-xs lg:text-sm font-body placeholder-body min-h-12  bg-white border-gray-300 focus:outline-none focus:border-heading  md:h-12  lg:px-7 h-12 lg:h-14 text-center ltr:sm:text-left rtl:sm:text-right  rounded-md"
+                  autocomplete="off"
+                  spellcheck="false"
+                  aria-invalid="false"
+                />
+              </div>
+             <div className="text-white bg-black px-7 py-4 rounded-md hover:bg-gray-800">
+             <Button
+                text="Subscribe"
+                icon={<i class="fa-solid fa-arrow-right-long"></i>}
+                type="submit"
+                link="/contact"
+              />
+             </div>
+            </div>
+          </form>
+          <div
+            style={style}
+            className="hidden z-0 xl:block bg-no-repeat bg-right xl:-right-24 2xl:-right-20 3xl:right-0 bg-contain xl:bg-cover 3xl:bg-contain absolute h-full w-full top-0"
+          ></div>
+        </div>
         </div>
         {/* footer*/}
-        <div className="my-2 p-5 bg-black">
-          <div className="flex bg-red-600 justify-between items-center w-[90%] m-auto">
-          <div className=" cursor-pointer px-5 w-[20%] bg-slate-200">
-          <img
-            className=" object-contain rounded-lg w-[95px] md:w-[200px] "
-            src="/assets/img/logo.svg"
-            alt="LOGO." 
-          />
-        </div>
-          <div className="flex justify-around items-center bg-slate-500 w-[80%]">
-          <div className=" flex flex-col p-2 gap-3">
-          <span><a href="">Facebook</a></span>
-          <span><a href="">Instagram</a></span>
-          <span><a href="">Youtube</a></span>
-          <span><a href="">TikTok</a></span>
-          </div>
-          <div className=" flex flex-col p-2 gap-3">
-          <span><a href="">Facebook</a></span>
-          <span><a href="">Instagram</a></span>
-          <span><a href="">Youtube</a></span>
-          <span><a href="">TikTok</a></span>
-          </div>
-          
-          </div>
-          </div>
+        <div className=" p-2">
+          <Footer />
         </div>
       </div>
       {/* <div className="container py-3 fixed bottom-0 bg-white">
